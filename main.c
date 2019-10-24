ef4f99d7014bb40af4388168b82479b461f93f4d
pjd321@nyu.edu <pjd321@nyu.edu>
2019-10-23 20:27:26 -0400

    Checkpoint commit, did alot

--- main.c
+++ main.c
@@ -5,21 +5,35 @@ int main(int argc, char *argv[]){
 		printf("Not enough or too many arguments, two expected (Document, Dictionary)\n");
 		exit(0);
 	}
-	
-	const char *text_file = argv[1];
 	const char *dict_file = argv[2];
+	const char *text_file = argv[1];
+
+	
 	hashmap_t hashtable[HASH_SIZE];
 	
 	load_dictionary(dict_file, hashtable);
 	
 	char *misspelled[MAX_MISSPELLED];
 	FILE *fp = fopen(text_file, "r");
-	
+	if(fp == NULL){
+		return 1;
+	}
 	int num_misspelled = check_words(fp, hashtable, misspelled);
 	printf("%d Mistakes\n", num_misspelled);
 	for(int i=0; i<num_misspelled; i++){
 		printf("%s\n", misspelled[i]);
+		free(misspelled[i]);
 	}
-	
+	for(int j=0; j<HASH_SIZE; j++){
+		while(hashtable[j]){
+			
+			hashmap_t next =hashtable[j]->next;
+			hashmap_t prev = hashtable[j];
+		
+			free(prev->word);
+			hashtable[j] = next;
+		}
+	}
+	fclose(fp);
 	return 0;
 }
--- spell.c
+++ spell.c
@@ -8,88 +8,100 @@
 int check_words(FILE* fp, hashmap_t hashtable[], char* misspelled[]){
 	
 	int num_misspelled = 0;
-	char *line;
+	//Initialized line value fixed one "Conditional jump or move depends on uninitialised value(s)" which occurs in while loop when checking values during getline
+	char *line = "";
 	size_t len = 0;
 	ssize_t read;
 	while(getline(&line, &len, fp) != -1){
 		char *word = NULL;
 		word = strtok(line, " ");
 		while(word != NULL){
-
 			if(word[strlen(word)-1] == '\n'){
 				word[strlen(word)-1] = '\0';
 			}
-
+			
 			if(word[strlen(word)-1] == '.' || word[strlen(word)-1] == '!' || word[strlen(word)-1] == '?' || word[strlen(word)-1] == ',' || word[strlen(word)-1] == ';'){
 				word[strlen(word)-1] = '\0';
 			}
 			
-			int bucket = hash_function(word);
+
 			if(strlen(word) < LENGTH+1){
-	  			if(check_word(word, hashtable) == false){
+
+	  			if(check_word(word, hashtable) == false && strcmp(word, "") !=0){
+
 					char *new_word = NULL;
 					new_word = malloc(LENGTH+1);
+
 					strcpy(new_word, word);
 	  				misspelled[num_misspelled] = new_word;
 	  				num_misspelled++;
-	  			}
+	  			} else{
+					
+				}
 		  		
 			}
 			word = strtok(NULL, " ");
 
   		}
+		//Addresses memory leak in creating char * to read words in line
+		free(word);
 		
 
 	}
-	
+	//Addresses memory leak in creating char * to read lines
+	free(line);
+		
 	return num_misspelled;
 	
 }
 
 bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]){
-
 	for(int i=0; i< HASH_SIZE; i++){		
 		hashtable[i] = NULL;
 	}
-	FILE *fpd = fopen(dictionary_file, "r");
+	FILE *fpd = fopen(dictionary_file, "r");	
 	if(fpd == NULL){
 		return false;
 	}
-	char *line;
+	//Initialized line value fixed one "Conditional jump or move depends on uninitialised value(s)" which occurs in while loop when checking values during getline, this seems to also fix the same problem with the conditional statement in the loop which checks the sizeof the line variable
+	char *line = "";
 	size_t len = 0;
 	ssize_t read;
-	int prev = 0 ;
 	while(getline(&line, &len, fpd) != -1){
-
-		char *word = NULL;
-		word = malloc(LENGTH+1);	
+		//Malloc 1024 chars fixed write size 8 issue
+		char *word = malloc(sizeof(char)*1024);	
 		hashmap_t new_node = {word, NULL};
+		//Fixes "Conditional jump or move depends on uninitialised value(s)" in check_word where the nodes "next" variable was not initialized as NULL in the above statement.
+		new_node->next = NULL;
 
 		if(line[strlen(line)-1] == '\n'){
 			line[strlen(line)-1] = '\0';
 		}
+		
 		if(strlen(line) < LENGTH+1){
+			
 			strcpy(new_node->word, line);
 			int bucket = hash_function(new_node->word);
-			
-			if(hashtable[bucket] == NULL){
+			//AFL generated input triggered a condition where the bucket number was negative adding additional checks
+			if(bucket >= 0 AND bucket <= HASH_LENGTH){
+				if(hashtable[bucket] == NULL){
 
-				hashtable[bucket] = new_node;
+					hashtable[bucket] = new_node;
 
-			} else{
-				
-				new_node->next = hashtable[bucket];
+				} else{
+					
+					new_node->next = hashtable[bucket];
 
-				hashtable[bucket] = new_node;
+					hashtable[bucket] = new_node;
 
+				}
 			}
 			
-			prev = bucket;
 		}
 	}
-
 	fclose(fpd);
-	
+	//Addresses memory leak in creating char * to read file lines
+	free(line);
 	return true;
 
 }
@@ -97,12 +109,20 @@ bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]){
 bool check_word(const char* word, hashmap_t hashtable[]){
 	
 	int bucket = hash_function(word);
+	//AFL generated input triggered a condition where the bucket number was -1064
+	if(bucket < 0){
+		return false;
+	}
 	hashmap_t cursor = hashtable[bucket];
 
 	while(cursor != NULL){
+
 		if(strcmp(word, cursor->word)==0){
+
 			return true;
+			
 		} else {
+
 			cursor = cursor->next;
 		}
 	}
@@ -111,17 +131,17 @@ bool check_word(const char* word, hashmap_t hashtable[]){
 	check_word[0] = tolower(check_word[0]);
 	check_word[strlen(check_word)-1] = tolower(check_word[strlen(check_word)-1]);
 	bucket = hash_function(check_word);
-
+	if(bucket < 0){
+		return false;
+	}
 	cursor = hashtable[bucket];
-
-	while(cursor != NULL){
+	while(cursor){
 		if(strcmp(check_word, cursor->word)==0){
 			return true;
 		} else{
 			cursor = cursor->next;
 		}
 	}
-	
 	
 	return false;
 }
